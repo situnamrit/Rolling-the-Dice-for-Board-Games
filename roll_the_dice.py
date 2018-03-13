90a8c3c5a15e5a44720b0972265b0df0a3c37ad6
Amrit Sahani <situnamrit@gmail.com>
2018-03-13 12:14:38 +0530

    Add files via upload

---
+++ roll_the_dice.py
@@ -0,0 +1,14 @@
+import random
+dice1 = [1,2,3,4,5,6]
+dice2 = [1,2,3,4,5,6]
+print(" DICE 1 is BEING ROLLED \n")
+random.shuffle(dice1)
+random.shuffle(dice2)
+choice = 'y'
+while choice == 'y' or choice == 'Y':
+     random.shuffle(dice1)
+     random.shuffle(dice2)
+     print(random.choice(dice1)+random.choice(dice2))
+     print("DO YOU WANT TO ROLL AGAIN \n")
+     choice = (input("ENTER YOUR CHOICE"))
+
